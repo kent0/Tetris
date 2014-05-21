7bb3098115e1178e6eef5d90cb2217251fff1b22
Kento Kaneko <kaneko2@illinois.edu>
2014-05-21 10:41:02 -0500

    Removed time keeping for now. Moved initialization to constuctor. Modified the depth of the well to 22 to compensate for the space of the tetrominos. Added code to initialize the position of the tetrominos.

--- Game.java
+++ Game.java
@@ -1,18 +1,44 @@
 import java.lang.String;
 
 public class Game {
-    long startTime;
-    int[][] well = new int[20][10];
-    Tetromino currentPiece = new Tetromino();
+    int[][] well;
+    Tetromino currentPiece;
 
     public Game() {
-	this.startTime = System.currentTimeMillis();
-
+	currentPiece = new Tetromino();
+	well = new int[22][10];
+	
 	for (int i = 0; i < well.length; i++) {
 	    for (int j = 0; j < well[0].length; j++) {
 		this.well[i][j] = 0;
 	    }
 	}
+
+	int[] startPosition = new int[2];
+	
+	switch (currentPiece.getShape().length) {
+	    
+	case 2:
+	    startPosition[0] = 1;
+	    startPosition[1] = 9;
+	    break;
+	    
+	case 3:
+	    startPosition[0] = 1;
+	    startPosition[1] = 8;
+	    break;
+
+	case 4:
+	    startPosition[0] = 0;
+	    startPosition[1] = 8;
+	    break;
+
+	default:
+	    break;
+	    
+	}
+
+	currentPiece.setPosition(startPosition);
     }
 
     public String toString() {
