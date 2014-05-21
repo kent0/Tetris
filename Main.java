ae7b3538507041b083fb65e1d8862c6349ea7228
Kento Kaneko <kaneko2@illinois.edu>
2014-05-21 18:10:00 -0500

    Replaced a JPanel object with a custom object that inherits from JPanel. Moved the game initiation code.

--- Main.java
+++ Main.java
@@ -1,28 +1,25 @@
 import java.awt.*;
 import javax.swing.*;
+import java.awt.event.*;
 
 public class Main {
+    
     public static void main(String[] args){
 	JFrame frame = new JFrame("Tetris");
 	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	frame.setSize(480,768);
 
-	JPanel panel = new JPanel();
+	Game game = new Game();
+
+	TPanel panel = new TPanel(game);
 	panel.setBackground(Color.YELLOW);
 	panel.setSize(360,720);
 
-	JLabel label = new JLabel("Test Label");
-	JButton button = new JButton();
-
-	panel.add(button);
-	panel.add(label);
 	frame.add(panel);
-
 	frame.setVisible(true);
 
 	Tetromino.test();
 	
-	Game game = new Game();
 	System.out.println(game.toString());
     }
 }
