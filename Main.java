b0669646f1b7187857703fa8f585f0491932ec03
Kento Kaneko <kaneko2@illinois.edu>
2014-05-21 06:30:17 -0500

    added a trial window system.

--- Main.java
+++ Main.java
@@ -1,6 +1,24 @@
+import java.awt.*;
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args){
-	System.out.println("This is a test.");
+	JFrame frame = new JFrame("Tetris");
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	frame.setSize(480,768);
+
+	JPanel panel = new JPanel();
+	panel.setBackground(Color.YELLOW);
+	panel.setSize(360,720);
+
+	JLabel label = new JLabel("Test Label");
+	JButton button = new JButton();
+
+	panel.add(button);
+	panel.add(label);
+	frame.add(panel);
+
+	frame.setVisible(true);
     }
 }
     
