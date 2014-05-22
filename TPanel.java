6ad770be6f9ac9826f3709de76557decf48fab17
Kento Kaneko <kaneko2@illinois.edu>
2014-05-21 23:12:51 -0500

    Clarified code. Use the well array in the game object instead of deprecated internal array.

--- TPanel.java
+++ TPanel.java
@@ -5,10 +5,10 @@ public class TPanel extends JPanel {
     int[] origin;
     final int wellWidth = 360;
     final int wellHeight = 720;
-    Game game;
+    Game gameCopy;
     
     public TPanel(Game game) {
-	this.game = game;
+	this.gameCopy = game;
 	origin = new int[]{0,0};
 	setBorder(BorderFactory.createLineBorder(Color.black));
     }
@@ -17,13 +17,11 @@ public class TPanel extends JPanel {
 	super.paintComponent(g);
 
        	g.drawRect(0,0,wellWidth,wellHeight);
-	int[][] well = new int[1][1];
-	if (well.length == 20 && well[0].length == 10) {
-	    for (int i = 0; i < 20; i++) {
-		for (int j = 0; j < 10; j++) {
-		    if (well[i][j] != 0) {
-			g.fillRect(origin[0]+36*i,origin[1]+36*j,36,36);
-		    }
+
+	for (int i = 0; i < 20; i++) {
+	    for (int j = 0; j < 10; j++) {
+		if (gameCopy.getwell()[i+2][j] != 0) {
+		    g.fillRect(origin[0]+36*(i+2),origin[1]+36*j,36,36);
 		}
 	    }
 	}
