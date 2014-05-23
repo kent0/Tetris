3f3aaffd7638b9e15631eacc274999b743e71abe
Kento Kaneko <kaneko2@illinois.edu>
2014-05-22 21:53:44 -0500

    Added a KeyListener to panel

--- Main.java
+++ Main.java
@@ -7,7 +7,7 @@ public class Main {
     static TPanel panel;
     static Game game;
     
-    public static void main(String[] args){
+    public static void main(String[] args) {
 	JFrame frame = new JFrame("Tetris");
 	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	frame.setSize(480,768);
@@ -17,6 +17,44 @@ public class Main {
 	panel = new TPanel(game);
 	panel.setBackground(Color.YELLOW);
 	panel.setSize(360,720);
+	panel.setFocusable(true);
+	panel.addKeyListener(new KeyListener() {
+
+		@Override
+		public void keyTyped(KeyEvent e) {
+		    	switch (e.getExtendedKeyCode()) {
+	    
+			case KeyEvent.VK_W:
+			    break;
+			    
+			case KeyEvent.VK_A:
+			    game.left();
+			    panel.repaint();
+			    break;
+			    
+			case KeyEvent.VK_S:
+			    game.down();
+			    panel.repaint();
+			    break;
+			    
+			case KeyEvent.VK_D:
+			    game.right();
+			    panel.repaint();
+			    break;
+			    
+			default:
+			    break;
+			}
+		}
+
+		@Override
+		public void keyReleased(KeyEvent e) {}
+
+		@Override
+		public void keyPressed(KeyEvent e) {
+		    System.out.println("Pressed " + e.getKeyChar());
+		}
+	    });
 
 	frame.add(panel);
 	frame.setVisible(true);
