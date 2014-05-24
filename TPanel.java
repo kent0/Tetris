2b4a17fe3871f1199c3d7f024cb3bd8b3b2f4586
Kento Kaneko <kaneko2@illinois.edu>
2014-05-24 17:21:32 -0500

    Removed labels. Created new Graphics2D objects.

--- TPanel.java
+++ TPanel.java
@@ -1,74 +1,74 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.RenderingHints;
 
 public class TPanel extends JPanel {
     private int[] origin;
-    private JLabel scoreLabel, lineLabel, levelLabel;
     private Game gameCopy;
     private int blockSize;
     
     public TPanel(Game game) {
-	scoreLabel = new JLabel("Score: ");
-	lineLabel = new JLabel("Line: ");
-	levelLabel = new JLabel("Level: ");
-
-	scoreLabel.setForeground(Color.LIGHT_GRAY);
-	lineLabel.setForeground(Color.LIGHT_GRAY);
-	levelLabel.setForeground(Color.LIGHT_GRAY);
-
-	this.add(scoreLabel);
-	this.add(lineLabel);
-	this.add(levelLabel);
-	
 	this.gameCopy = game;
-	this.blockSize = 36;
-	this.origin = new int[]{this.blockSize,this.blockSize};
-	this.setSize(10*this.blockSize,20*this.blockSize);
+	this.blockSize = 24;
+	this.origin = new int[]{this.blockSize*2,this.blockSize*3};
+	this.setSize(16*this.blockSize,20*this.blockSize);
     }
 
     public void paintComponent(Graphics g) {
 	super.paintComponent(g);
+
+	Graphics2D g2 = (Graphics2D) g;
+	
 	this.setBackground(Color.BLACK);
 	System.out.println(gameCopy);
 
-	g.setColor(Color.DARK_GRAY);
-       	g.drawRect(this.origin[0],this.origin[1],10*this.blockSize,20*this.blockSize);
+	g2.setColor(Color.DARK_GRAY);
+       	g2.drawRect(this.origin[1],this.origin[0],10*this.blockSize,20*this.blockSize);
 
 	for (int i = 0; i < 20; i++) {
 	    for (int j = 0; j < 10; j++) {
 		if (gameCopy.getWell()[i+2][j] != 0) {
 		    
-		    g.setColor(this.brushColor(this.gameCopy.getWell()[i+2][j]));
-		    g.fillRect(origin[1]+this.blockSize*(j),origin[0]+this.blockSize*i,this.blockSize,this.blockSize);
+		    g2.setColor(this.brushColor(this.gameCopy.getWell()[i+2][j]));
+		    g2.fillRect(origin[1]+this.blockSize*(j),origin[0]+this.blockSize*i,this.blockSize,this.blockSize);
 		}
 	    }
 	}
 
-	g.setColor(Color.DARK_GRAY);
+	g2.setColor(Color.DARK_GRAY);
 	
 	for (int i = 0; i < 20; i++) {
-	    g.drawLine(origin[1],origin[0]+this.blockSize*i,origin[1]+10*this.blockSize,origin[0]+this.blockSize*i);
+	    g2.drawLine(origin[1],origin[0]+this.blockSize*i,origin[1]+10*this.blockSize,origin[0]+this.blockSize*i);
 	}
 
 	for (int i = 0; i < 10; i++) {
-	    g.drawLine(origin[1]+this.blockSize*i,origin[0],origin[1]+this.blockSize*i,origin[0]+20*this.blockSize);
+	    g2.drawLine(origin[1]+this.blockSize*i,origin[0],origin[1]+this.blockSize*i,origin[0]+20*this.blockSize);
 	}
 
 	for (int i = 1; i < 4; i++) {
-	    g.setColor(this.brushColor(this.gameCopy.getTetrominos()[i].type()));
+	    g2.setColor(this.brushColor(this.gameCopy.getTetrominos()[i].type()));
 	    for (int j = 0; j < 4; j++) {
-		g.fillRect(this.origin[1]+this.blockSize*10+this.blockSize*(this.gameCopy.getTetrominos()[i].configuration()[j][1]+1)/2,(i-1)*this.blockSize*3+this.origin[0]+this.blockSize*this.gameCopy.getTetrominos()[i].configuration()[j][0]/2,this.blockSize/2,this.blockSize/2);
+		g2.fillRect(this.origin[1]+this.blockSize*10+this.blockSize*(this.gameCopy.getTetrominos()[i].configuration()[j][1]+1)/2,(i-1)*this.blockSize*3+this.origin[0]+this.blockSize*this.gameCopy.getTetrominos()[i].configuration()[j][0]/2,this.blockSize/2,this.blockSize/2);
 	    }
 	}
 
-	g.setColor(this.brushColor(this.gameCopy.getTetrominos()[this.gameCopy.getTetrominos().length-1].type()));
+	g2.setColor(this.brushColor(this.gameCopy.getTetrominos()[this.gameCopy.getTetrominos().length-1].type()));
 
 	for (int j = 0; j < 4; j++) {
-	    g.fillRect(this.origin[1]+this.blockSize*10+this.blockSize*(this.gameCopy.getTetrominos()[4].configuration()[j][1]+1)/2,(4-1)*this.blockSize*3+this.origin[0]+this.blockSize*this.gameCopy.getTetrominos()[4].configuration()[j][0]/2,this.blockSize/2,this.blockSize/2);
+	    g2.fillRect(this.blockSize*(this.gameCopy.getTetrominos()[4].configuration()[j][1]+1)/2,this.origin[0]+this.blockSize*this.gameCopy.getTetrominos()[4].configuration()[j][0]/2,this.blockSize/2,this.blockSize/2);
 	    if (this.gameCopy.getTetrominos()[4].type() == 0) {
 		break;
 	    }
 	}
+
+	g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+	
+	g2.setFont(new Font("Futura", Font.PLAIN, 14));
+
+	g2.setColor(Color.WHITE);
+	g2.drawString("Hold", this.blockSize, this.blockSize);
+	g2.drawString("Score: " + this.gameCopy.getScore(), this.blockSize * 6, this.blockSize);
+	g2.drawString("Next", this.blockSize * 14, this.blockSize);
 
     }
     
