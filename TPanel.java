e64b943c7ee4a6d298e5418e575c43fde336ab32
Kento Kaneko <kaneko2@illinois.edu>
2014-05-23 22:04:40 -0500

    Removed deprecated variables. Changed the label colors. Moved setting background color. Created a method that is common between two parts of the code.

--- TPanel.java
+++ TPanel.java
@@ -3,8 +3,6 @@ import java.awt.*;
 
 public class TPanel extends JPanel {
     private int[] origin;
-    private int wellWidth = 360;
-    private int wellHeight = 720;
     private JLabel scoreLabel, lineLabel, levelLabel;
     private Game gameCopy;
     private int blockSize;
@@ -14,9 +12,9 @@ public class TPanel extends JPanel {
 	lineLabel = new JLabel("Line: ");
 	levelLabel = new JLabel("Level: ");
 
-	scoreLabel.setForeground(Color.DARK_GRAY);
-	lineLabel.setForeground(Color.DARK_GRAY);
-	levelLabel.setForeground(Color.DARK_GRAY);
+	scoreLabel.setForeground(Color.LIGHT_GRAY);
+	lineLabel.setForeground(Color.LIGHT_GRAY);
+	levelLabel.setForeground(Color.LIGHT_GRAY);
 
 	this.add(scoreLabel);
 	this.add(lineLabel);
@@ -25,12 +23,12 @@ public class TPanel extends JPanel {
 	this.gameCopy = game;
 	this.blockSize = 36;
 	this.origin = new int[]{this.blockSize,this.blockSize};
-	this.setBackground(Color.BLACK);
 	this.setSize(10*this.blockSize,20*this.blockSize);
     }
 
     public void paintComponent(Graphics g) {
 	super.paintComponent(g);
+	this.setBackground(Color.BLACK);
 	System.out.println(gameCopy);
 
 	g.setColor(Color.DARK_GRAY);
@@ -39,48 +37,8 @@ public class TPanel extends JPanel {
 	for (int i = 0; i < 20; i++) {
 	    for (int j = 0; j < 10; j++) {
 		if (gameCopy.getWell()[i+2][j] != 0) {
-		    Color brushColor;
 		    
-		    switch (gameCopy.getWell()[i+2][j]) {
-			
-		    case -1:
-			brushColor = Color.LIGHT_GRAY;
-			break;
-			
-		    case 1:
-			brushColor = Color.CYAN;
-			break;
-	
-
-		    case 2:
-			brushColor = Color.YELLOW;
-			break;
-
-		    case 3:
-			brushColor = Color.MAGENTA;
-			break;
-
-		    case 4:
-			brushColor = Color.BLUE;
-			break;
-
-		    case 5:
-			brushColor = Color.ORANGE;
-			break;
-
-		    case 6:
-			brushColor = Color.GREEN;
-			break;
-
-		    case 7:
-			brushColor = Color.RED;
-			break;
-			
-		    default:
-			brushColor = Color.BLACK;
-		    }
-		    
-		    g.setColor(brushColor);
+		    g.setColor(this.brushColor(this.gameCopy.getWell()[i+2][j]));
 		    g.fillRect(origin[1]+this.blockSize*(j),origin[0]+this.blockSize*i,this.blockSize,this.blockSize);
 		}
 	    }
@@ -96,10 +54,45 @@ public class TPanel extends JPanel {
 	    g.drawLine(origin[1]+this.blockSize*i,origin[0],origin[1]+this.blockSize*i,origin[0]+20*this.blockSize);
 	}
 
-	for (int i = 0; i < 3; i++) {
-	    
+	for (int i = 1; i < 4; i++) {
+	    g.setColor(this.brushColor(this.gameCopy.getTetrominos()[i].type()));
+	    for (int j = 0; j < 4; j++) {
+		g.fillRect(this.origin[1]+this.blockSize*10+this.blockSize*(this.gameCopy.getTetrominos()[i].configuration()[j][1]+1)/2,(i-1)*this.blockSize*3+this.origin[0]+this.blockSize*this.gameCopy.getTetrominos()[i].configuration()[j][0]/2,this.blockSize/2,this.blockSize/2);
+	    }
 	}
-					 
+    }
+
+    public Color brushColor(int brushType) {
+		    
+	switch (brushType) {
+			
+	case -1:
+	    return Color.LIGHT_GRAY;
+			
+	case 1:
+	    return Color.CYAN;
+   
+	case 2:
+	    return Color.YELLOW;
+
+	case 3:
+	    return Color.MAGENTA;
+
+	case 4:
+	    return Color.BLUE;
+
+	case 5:
+	    return Color.ORANGE;
+
+	case 6:
+	    return Color.GREEN;
+
+	case 7:
+	    return Color.RED;
+			
+	default:
+	    return Color.BLACK;
+	}
     }
 
     public void update() {
