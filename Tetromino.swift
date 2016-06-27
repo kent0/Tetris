dea2bcc3a442dd05beacd9e40cddb133f7819699
Kento Kaneko <kaneko2@illinois.edu>
2016-06-26 20:40:34 -0500

    implemented block() -> [[Bool]] method

--- Tetromino.swift
+++ Tetromino.swift
@@ -3,14 +3,36 @@ import Darwin
 enum Tetromino: Int {
 	case I = 0, O, T, L, J, S, Z
 
-	// func block() -> [[Bool]] {
-	// 	switch Self {
-	// 		case: .I
-	// 		case: .O
-	// 		case: .T
-	// 		case: .L
-	// 		case: .S
-	// 		case: .Z
-	// 	}
-	// }
+	func block() -> [[Bool]] {
+		switch self {
+			case .I:
+				return [[false, false, false, false],
+						[false, false, false, false],
+						[ true,  true,  true,  true],
+						[false, false, false, false]]
+			case .O:
+				return [[true, true],
+						[true, true]]
+			case .T:
+				return [[false,  true, false],
+						[ true,  true,  true],
+						[false, false, false]]
+			case .J:
+				return [[ true, false, false],
+						[ true,  true,  true],
+						[false, false, false]]
+			case .L:
+				return [[false, false,  true],
+						[ true,  true,  true],
+						[false, false, false]]
+			case .S:
+				return [[false,  true,  true],
+						[ true,  true, false],
+						[false, false, false]]
+			case .Z:
+				return [[ true,  true, false],
+						[false,  true,  true],
+						[false, false, false]]
+		}
+	}
 }
