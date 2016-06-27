50eed6cb6b2be74c20a58fdcab861afba2ecb1ec
Kento Kaneko <kaneko2@illinois.edu>
2016-06-26 21:04:05 -0500

    converted tab to spaces

--- Tetromino.swift
+++ Tetromino.swift
@@ -1,38 +1,38 @@
 import Darwin
 
 enum Tetromino: Int {
-	case I = 0, O, T, L, J, S, Z
+    case I = 0, O, T, L, J, S, Z
 
-	func block() -> [[Bool]] {
-		switch self {
-			case .I:
-				return [[false, false, false, false],
-						[false, false, false, false],
-						[ true,  true,  true,  true],
-						[false, false, false, false]]
-			case .O:
-				return [[true, true],
-						[true, true]]
-			case .T:
-				return [[false,  true, false],
-						[ true,  true,  true],
-						[false, false, false]]
-			case .J:
-				return [[ true, false, false],
-						[ true,  true,  true],
-						[false, false, false]]
-			case .L:
-				return [[false, false,  true],
-						[ true,  true,  true],
-						[false, false, false]]
-			case .S:
-				return [[false,  true,  true],
-						[ true,  true, false],
-						[false, false, false]]
-			case .Z:
-				return [[ true,  true, false],
-						[false,  true,  true],
-						[false, false, false]]
-		}
-	}
+    func block() -> [[Bool]] {
+        switch self {
+            case .I:
+                return [[false, false, false, false],
+                        [false, false, false, false],
+                        [ true,  true,  true,  true],
+                        [false, false, false, false]]
+            case .O:
+                return [[true, true],
+                        [true, true]]
+            case .T:
+                return [[false,  true, false],
+                        [ true,  true,  true],
+                        [false, false, false]]
+            case .J:
+                return [[ true, false, false],
+                        [ true,  true,  true],
+                        [false, false, false]]
+            case .L:
+                return [[false, false,  true],
+                        [ true,  true,  true],
+                        [false, false, false]]
+            case .S:
+                return [[false,  true,  true],
+                        [ true,  true, false],
+                        [false, false, false]]
+            case .Z:
+                return [[ true,  true, false],
+                        [false,  true,  true],
+                        [false, false, false]]
+        }
+    }
 }
