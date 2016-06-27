e53c0e850422e6498d386023f75aae12e935acbe
Kento Kaneko <kaneko2@illinois.edu>
2016-06-26 19:57:44 -0500

    added swifty tetromino

---
+++ Tetromino.swift
@@ -0,0 +1,16 @@
+import Darwin
+
+enum Tetromino: Int {
+	case I = 0, O, T, L, J, S, Z
+
+	// func block() -> [[Bool]] {
+	// 	switch Self {
+	// 		case: .I
+	// 		case: .O
+	// 		case: .T
+	// 		case: .L
+	// 		case: .S
+	// 		case: .Z
+	// 	}
+	// }
+}
